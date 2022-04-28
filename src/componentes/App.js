76980b23b4bab2fbbe684bbf540acb6c5731a489
zai-alvarez <zai.alvarez.l@gmail.com>
2022-04-28 10:38:50 +0200

    template

---
+++ src/componentes/App.js
@@ -0,0 +1,15 @@
+import logo from '../images/logo.svg';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, quasi. Dolores incidunt impedit officia consequatur voluptatum, dolor fugit veritatis exercitationem? Numquam quasi, quam quibusdam adipisci eligendi reprehenderit quaerat eum animi.
+      </p>
+    </div >
+  );
+}
+
+export default App;
